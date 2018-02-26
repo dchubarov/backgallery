cd6f22ba10a666783901b25fee309937fd100733
Dmitry Chubarov <dime@twowls.org>
2018-02-26 20:35:28 +0300

    Create initial project structure.

---
+++ src/main/java/org/twowls/backgallery/Backgallery.java
@@ -0,0 +1,14 @@
+package org.twowls.backgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dmitry Chubarov
+ */
+@SpringBootApplication
+public class Backgallery {
+    public static void main(String[] args) {
+        SpringApplication.run(Backgallery.class, args);
+    }
+}
